d3e19615718d56f192efc15f46c012ffcb1aba19
Krzysztof Gawlinski <krg@bec.dk>
2025-11-06 10:50:30 +0100

    update of configuration for orgmode

--- neovim/.config/nvim/lua/plugins/orgmode.lua
+++ neovim/.config/nvim/lua/plugins/orgmode.lua
@@ -5,8 +5,8 @@ return {
   config = function()
     -- Setup orgmode
     require('orgmode').setup({
-      org_agenda_files = '~/orgfiles/**/*',
-      org_default_notes_file = '~/orgfiles/refile.org',
+      org_agenda_files = '~/Notes/*',
+      org_default_notes_file = '~/Notes/main.org',
     })
 
     -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
